0c24185a57250b3622f449d91a260ab336adc5e1
hhtun137 <htunaunghlaing199@gmail.com>
2024-03-04 19:25:06 +0630

    this&target pointcut designators with proxy objects

--- src/test/java/com/HAH/demo/test/TestDemo.java
+++ src/test/java/com/HAH/demo/test/TestDemo.java
@@ -3,23 +3,22 @@ package com.HAH.demo.test;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-
-import com.HAH.demo.admin.service.AdminService;
+import com.HAH.demo.HelloEnable;
 import com.HAH.demo.member.service.MemberService;
 
 @SpringJUnitConfig(locations = "classpath:app.xml")
 public class TestDemo {
 	
 	@Autowired
-	private AdminService adminService;
+	private HelloEnable helloEnable;
 	
 	@Autowired
 	private MemberService memberService;
 	
 	@Test
 	void demoTest() {
-//		adminService.helloMethod("Hello Learning AOP Concept");
-		memberService.memberMethods("Htun Aung Hlaing", 23);
+		helloEnable.helloMethod("Hello Learning AOP Concept");
+//		memberService.memberMethods("Htun Aung Hlaing", 23);
 	}
 
 }
